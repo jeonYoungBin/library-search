aabed46dcbc24a7fab29325f69ec2a72ca4cda70
jeon-yongbin <cjswkdl@naver.com>
2025-05-06 16:48:06 +0900

    Revert "[LS-9] 고가용성설계 - circuit breaker, kakao-client 적용"
    This reverts commit 4696c553833f3fb70b6c6af85e76127e6ca368a2.

--- search-api/src/main/java/org/search/LibararySearchApplication.java
+++ search-api/src/main/java/org/search/LibararySearchApplication.java
@@ -1,12 +1,11 @@
 package org.search;
 
-import org.search.feign.KakaoClient;
 import org.search.feign.NaverClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(clients = {NaverClient.class, KakaoClient.class})
+@EnableFeignClients(clients = NaverClient.class)
 @SpringBootApplication
 public class LibararySearchApplication {
     public static void main(String[] args) {
